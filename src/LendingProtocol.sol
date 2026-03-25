61091fd0b9affd42b3259c1dfeb2d140e211cb39
Alejo Ezequiel Vilches <alejoevilches@gmail.com>
2026-03-24 22:47:52 -0300

    create bidLoan

--- src/LendingProtocol.sol
+++ src/LendingProtocol.sol
@@ -11,29 +11,49 @@ contract LendingProtocol{
     };
 
     struct Loan{
-        uint256 id
-        address owner
-        LendingState status
-        uint256 amount
-        uint256 startDate
+        uint256 id,
+        address owner,
+        LendingState status,
+        uint256 amount,
+        uint256 startDate,
         uint256 dueDate
     }
 
+    struct Bid{
+        uint256 id,
+        uint256 loanId,
+        uint256 interest,
+    }
+
+    error BidLoan_LoanNotInBiddingStatus;
+
     Loan[] private loansCollection;
     uint256 loanCounter;
+    uint256 bidCounter;
 
     mapping(uint256=>Loan) idToLoan;
+    mappint(uint256=>Bid) idToBid;
 
     function createLoan(uint256 amount){
-        raffleCollection.push(
-            Loan({
-                id: loanCounter
-                address: msg.sender,
-                amount,
-                status: LendingState.REQUESTED
-            })
-        )
-        userToLoan[loanCounter] = loanCounter;
-        loanCounte++;
+        Loan payload = Loan({
+            id: loanCounter,
+            address: msg.sender,
+            amount,
+            status: LendingState.REQUESTED
+        });
+        loansCollection.push(payload);
+        idToLoan[loanCounter] = payload;
+        loanCounter++;
+    }
+
+    function bidLoan(uint256 loanId, uint256 interest){
+
+        Bid payload = Bid({
+            id: bidCounter,
+            loanID,
+            interest
+        });
+        idToBid[bidCounter] = payload;
+        bidCounter++;
     }
 }
